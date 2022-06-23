68dc9e482b9465c09013642da4db1f53a54ba1b0
edmasoom <edmasoom@gmail.com>
2022-06-22 20:27:47 -0700

    first commit

---
+++ src/DayOne/GitHubPractice.java
@@ -0,0 +1,13 @@
+package DayOne;
+
+public class GitHubPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the end of project");
+		
+		System.out.println("Another test");
+		System.out.println("Test update");
+	}
+
+}
